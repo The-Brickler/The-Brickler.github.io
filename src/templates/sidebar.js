f9b06b161631481529ce475350f14450669a5e40
The-Brickler <cbrun.mine@gmail.com>
2023-09-28 12:51:36 -0600

    Attempted to streamline sidebar

---
+++ src/templates/sidebar.js
@@ -0,0 +1,24 @@
+class Sidebar extends HTMLElement
+{
+    constructor()
+    {
+        super();
+    }
+
+    connectedCallback()
+    {
+    this.innerHTML = `
+        <div class="sidebar">
+            <h2>Info</h2>
+                <a href="/index.html">About Me</a>
+            <h2>Projects</h2>
+                <a href="/src/html/mods.html">Minecraft Mods</a>
+                <a href="/src/html/programs.html">Programs</a>
+                <a href="/src/html/music.html">Music</a>
+        </div>
+        `;
+
+    }
+}
+
+customElements.define("sidebar", Sidebar);
